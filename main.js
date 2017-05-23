875594d9858b7e80a9693058a3ea1226d38be304
Tyler <tylersyme@gmail.com>
2017-05-23 12:41:00 -0700

    Created project base

---
+++ main.js
@@ -0,0 +1,44 @@
+
+var totalCreeps = 0;
+
+module.exports.loop = function() {
+
+	var spawns = Game.spawns;
+	var creeps = Game.creeps;
+	var source;
+
+	for (var name in creeps)
+	{
+		var creep = creeps[name];
+		var spawn = spawns['Spawn1'];
+		source = creep.room.find(FIND_SOURCES)[Math.floor(totalCreeps / 4)];
+
+		if (_.sum(creep.carry) < creep.carryCapacity)
+		{
+			if (creep.harvest(source) == ERR_NOT_IN_RANGE)
+			{
+				creep.moveTo(source);
+			}
+		}
+		else
+		{
+			if (creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+			{
+				creep.moveTo(spawn);
+			}
+		}
+	}
+
+	for (var spawnName in spawns)
+	{
+		var spawner = spawns[spawnName];
+
+		// The spawn will create new creeps if its energy exceeds 75% of its
+		// max carryCapacity
+		if (spawner.energy >= spawner.energyCapacity * .75)
+		{
+			++totalCreeps;
+			spawner.createCreep([WORK,CARRY,MOVE,MOVE], 'Harvester' + totalCreeps);
+		}
+	}
+};
