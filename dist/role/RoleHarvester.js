4cf2b411b9f218fa099ebbb8a6a23a913fd7ad56
LAPTOP-QU57SABP\tyler <tylersyme@gmail.com>
2017-05-27 19:26:01 -0700

    Added to role harvester

--- dist/role/RoleHarvester.js
+++ dist/role/RoleHarvester.js
@@ -1,11 +1,12 @@
 var Role = require('Role');
 var RoleType = require('RoleType');
+var StateType = require('StateType');
 var Priority = require('Priority');
 
 var RoleHarvester = function(creep, basePriority)
 {
   // Inherits all of Role's properties
-  Role.call(this, creep, RoleType.HARVESTER);
+  Role.call(this, creep, RoleType.HARVESTER, []);
 
   this.basePriority = basePriority;
 };
@@ -24,30 +25,69 @@ RoleHarvester.prototype.resolvePriority = function()
   return (1/distance) * this.basePriority;
 };
 
+/**
+ * Every role has different states it may be in. This will determine what state
+ * the role should be in during this particular tick.
+ * @return {State} The state which was chosen.
+ */
 RoleHarvester.prototype.resolveState = function()
 {
-  
+  var resolvedState;
+  var stateType = this.activeState.type;
+  if (this.activeState.isCompleted())
+  {
+    if (stateType === StateType.MOVING.SOURCE)
+    {
+      resolvedState = new StateHarvest(this.creep, this, this.activeState.source);
+    }
+    else if (stateType === StateType.BUILDING.SPAWN)
+    {
+      resolvedState = new StateTransfer(this.creep, this, this.activeState.spawn);
+    }
+    else if (stateType === StateType.HARVESTING)
+    {
+      var resolvedSpawn = this.resolveSpawn();
+      resolvedState = new StateMove(this.creep, this, this.activeState, resolvedSpawn);
+    }
+    else if (stateType === StateType.TRANSFERING || stateType === StateType.UNDEFINED)
+    {
+      var closestEnergySource = this.creep.room.find(FIND_SOURCES)[0];
+      resolvedState = new StateMove(this.creep, this, closestEnergySource);
+    }
+  }
+
+  return resolvedState;
+};
+
+RoleHarvester.prototype.resolveSpawn = function()
+{
+  var dominantSpawn;
+  var dominantSpawnWeight;
+
+  var creepRoom = this.creep.room;
+  var weight;
+  var spawn;
+  for (var spawnName in creepRoom.spawns)
+  {
+    spawn = creepRoom.spawns[spawnName];
+    var distance = this.creep.getRangeTo(spawn);
+
+    weight = spawn.energy / (distance);
+
+    if (weight > dominantSpawnWeight)
+    {
+      dominantSpawn = spawn;
+      dominantSpawnWeight = weight;
+    }
+  }
+
+  return spawn;
 };
 
 RoleHarvester.prototype.execute = function(state = undefined)
 {
-
-  var spawn = Game.spawns['Spawn1'];
-  var source = this.creep.room.find(FIND_SOURCES)[0];
-  if (_.sum(this.creep.carry) < this.creep.carryCapacity)
-	{
-		if (this.creep.harvest(source) == ERR_NOT_IN_RANGE)
-		{
-			this.creep.moveTo(source);
-		}
-	}
-	else
-	{
-		if (this.creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
-		{
-			this.creep.moveTo(spawn);
-		}
-	}
+  this.activeState = (state !== undefined) ? (this.resolveState()) : (state);
+  this.activeState.execute();
 };
 
 module.exports = RoleHarvester;
