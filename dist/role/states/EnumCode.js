af2e7929d0682677ff3e9df9a4d39846ca5dc818
Tyler <ternarytyler@gmail.com>
2017-05-31 01:16:45 -0700

    Implemented EnumCode with StateType
    Created EnumCode for multi-tier enumerations
    Reassigned StateType values with EnumCodes

---
+++ dist/role/states/EnumCode.js
@@ -0,0 +1,62 @@
+/**
+ * An EnumCode represents multi-tier enumeration numbers and their
+ *   relations to other enums. Using a version-like system (e.i. '1.4.2'),
+ *   one can see that the enum with this EnumCode is a grandchild of 1, 
+ *   a child of 4, and the 2nd sibliing. Other types can also be used to
+ *   represent relations such as String.
+ *
+ * @class      EnumCode
+ * 
+ * @param      {Array}  code    The array holding the multi-tier enumeration
+ *                              data
+ */
+var EnumCode = function(code) 
+{
+  this.code = code;
+}
+
+/**
+ * Returns if given EnumCode is equivalent to current EnumCodee
+ *
+ * @param      {EnumCode} other   The EnumCode to be compared
+ * @param      {number}   level   The max level to be checked (optional)
+ * 
+ * @return     {boolean}  False if any value within code and <= level is not
+ *                          equivalent; true otherwise
+ */
+EnumCode.prototype.equals = function(other, level)
+{
+  if (level == undefined)  // aka not given
+  {
+    level = Math.min(this.code.length, other.code.length) - 1;
+  }
+  else if (typeof other === 'EnumCode')  // other is of wrong type
+  {
+    return false;
+  }
+
+  thisCode = this.code.slice(0, level + 1);
+  otherCode = other.code.slice(0, level + 1);
+
+  return codeMatch(thisCode, otherCode);
+}
+
+/**
+ * Returns if two given codes match exactly
+ *
+ * @param      {array}  thisCode  The first code to be compared
+ * @param      {array}  thatCode  The second code to be compared
+ * 
+ * @return     {boolean}  False if not equal lengths or if any value 
+ *                          is not equivalent; true otherwise
+ */
+EnumCode.codeMatch = function(thisCode, thatCode)
+{
+  return thisCode.length == thatCode.length &&
+         thisCode.every(function(value, index) 
+         {
+           return value === thatCode[index];
+         })
+}
+
+module.exports = EnumCode;
--- dist/role/states/StateType.js
+++ dist/role/states/StateType.js
@@ -1,30 +1,91 @@
 var StateType =
 {
-  NONE : 0,
-  MOVING : 1,
-  HARVESTING : 2,
-  TRANSFERRING : 3,
-  BUILDING :
+  NONE   : EnumCode([0]),
+  MOVING :
   {
-    CONTAINER  : 4.01,
-    CONTROLLER : 4.02,
-    EXTENSION  : 4.03,
-    EXTRACTOR  : 4.04,
-    KEEPERLAIR : 4.05,
-    SPAWN      : 4.15
-  },
-  ATTACKING :
-  {
-    CREEP : 5.01,
-    STRUCTURE : {
-      CONTAINER  : 5.0201,
-      CONTROLLER : 5.0202,
-      EXTENSION  : 5.0203,
-      EXTRACTOR  : 5.0204,
-      KEEPERLAIR : 5.0205,
-      SPAWN      : 5.0215
+    GENERIC   : EnumCode([1])
+    POSITION  : EnumCode([1, 1]),
+    STRUCTURE : 
+    {
+      GENERIC    : EnumCode([1, 2])
+      CONTAINER  : EnumCode([1, 2, 1]),
+      CONTROLLER : Enumcode([1, 2, 2]),
+      EXTENSION  : EnumCode([1, 2, 3]),
+      EXTRACTOR  : EnumCode([1, 2, 4]),
+      KEEPERLAIR : EnumCode([1, 2, 5]),
+      SPAWN      : EnumCode([1, 2, 15])
+    },
+    RESOURCE : 
+    {
+      GENERIC : EnumCode([1, 3])
+      ENERGY  : EnumCode([1, 3, 1]),
+      POWER   : EnumCode([1, 3, 2]),
+      MINERAL : 
+      {
+        GENERIC  : EnumCode([1, 3, 3])
+        HYDROGEN : EnumCode([1, 3, 3, 1])
+      }
     }
+  },
+  HARVESTING : 
+  {
+    GENERIC  : EnumCode([2]),
+    RESOURCE : 
+    {
+      GENERIC : EnumCode([2, 1])
+      ENERGY  : EnumCode([2, 1, 1]),
+      POWER   : EnumCode([2, 1, 2]),
+      MINERAL : 
+      {
+        GENERIC  : EnumCode([2, 1, 3]),
+        HYDROGEN : EnumCode([2, 1, 3, 1])
+      }
+    }
+  },
+  TRANSFERRING :
+  {
+    GENERIC   : EnumCode([3]),
+    CREEP     : EnumCode([3, 1]),
+    STRUCTURE : 
+    {
+      GENERIC    : EnumCode([3, 2])
+      CONTAINER  : EnumCode([3, 2, 1]),
+      CONTROLLER : Enumcode([3, 2, 2]),
+      EXTENSION  : EnumCode([3, 2, 3]),
+      EXTRACTOR  : EnumCode([3, 2, 4]),
+      KEEPERLAIR : EnumCode([3, 2, 5]),
+      SPAWN      : EnumCode([3, 2, 15])
+    },
+  },
+  BUILDING : 
+  {
+    GENERIC   : EnumCode([4])
+    STRUCTURE : 
+    {
+      GENERIC    : EnumCode([4, 1])
+      CONTAINER  : EnumCode([4, 1, 1]),
+      CONTROLLER : Enumcode([4, 1, 2]),
+      EXTENSION  : EnumCode([4, 1, 3]),
+      EXTRACTOR  : EnumCode([4, 1, 4]),
+      KEEPERLAIR : EnumCode([4, 1, 5]),
+      SPAWN      : EnumCode([4, 1, 15])
+    },
+  },
+  ATTACKING : 
+  {
+    GENERIC : EnumCode([5])
+    CREEP   : EnumCode([5, 1]),
+    STRUCTURE : 
+    {
+      GENERIC    : EnumCode([5, 2])
+      CONTAINER  : EnumCode([5, 2, 1]),
+      CONTROLLER : Enumcode([5, 2, 2]),
+      EXTENSION  : EnumCode([5, 2, 3]),
+      EXTRACTOR  : EnumCode([5, 2, 4]),
+      KEEPERLAIR : EnumCode([5, 2, 5]),
+      SPAWN      : EnumCode([5, 2, 15])
+    },
   }
-};
+}
 
 module.exports = StateType;
